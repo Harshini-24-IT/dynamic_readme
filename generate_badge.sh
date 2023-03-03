5840b7d17a2774aedb76517dcc25095d9011df28
Harshini <65531498+Harshini-24-IT@users.noreply.github.com>
2023-03-03 10:39:44 +0530

    Update generate_badge.sh

--- generate_badge.sh
+++ generate_badge.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Parse the values from the txt file
-value=$(grep 'value:' timestamp.txt | awk '{print $2}')
+value=$(grep 'Timestamp:' timestamp.txt | awk '{print $2}')
 
 # Generate the badge URL using Shields.io
 badge_url="https://img.shields.io/badge/value-$value-green.svg"
